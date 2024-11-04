4c903a46fab992783b01f50ce2f3db1be44f9d39
jimmyboy722 <jeckman722@gmail.com>
2024-11-03 23:32:24 -0500

    added dependencies to install tailwindcss for styling, added pics, added a constant folder for my projects sect

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-    <header className="App-header">
-      
-    </>
+    <div>   
+       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  </div>
   )
 }
 
---
+++ src/constants/index.js
@@ -0,0 +1,34 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate brand new full stack developer with a knack for creating efficient and scalable web applications. Within the 6 months of hands-on experience I gained through the Bootcamp at Columbia Engineering, I have honed my skills in front-end technologies like React, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to learn from those who have been in the industry for years and work with them to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated, hard working and versatile full stack software engineer with a passion for creating efficient and user-friendly web applications along with an eye for creativity. Throughout my Columbia Engineering Bootcamp experience, I have worked with a variety of technologies, including React, Node.js, Express.js, MySQL, PostgreSQL, and MongoDB. My interest in web development began similarly to my career as an auto service technician... With a deep curiosity for how things work, and I'm hoping it will evolve into a career where I continue to strive to learn and adapt to new challenges. I excel in collaborative work environments and enjoy solving complex problems to deliver high-quality solutions. In addition to coding, I enjoy working out, exploring new technologies, and looking through open-source projects to improve my skills.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Senior Full Stack Developer",
+    company: "Google Inc.",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+];
+
+export const CONTACT = {
+  address: "767 Fifth Avenue, New York, NY 10153 ",
+  phoneNo: "+12 4555 666 00 ",
+  email: "me@example.com",
+};
