5c625f8f4b50c65e6f7071f5a1808ff16027a68d
jimmyboy722 <jeckman722@gmail.com>
2024-11-05 01:50:39 -0500

    added photos to assets, made navbar a nd hero components with tailwind css styling and added them to app.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,24 @@
-import '../styles/Navbar.css';
+// IMPORTS
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
 
-const styles = {
-    navbarStyle: {
-        background: 'red',
-        justifyContent: 'center',
-    },
-    headingStyle: {
-        fontSize: '50px',
-}
-};
-
-function Navbar() {
-    return (
-        <nav style={styles.navbarStyle} className="navbar">
-            <a href="#">Home</a>
-            <a href="#">About Me</a>
-            <a href="#">Projects</a>
-            <a href="#">Contact</a>
-        </nav>
-    );
+const Navbar = () => {
+  return (
+    // NAVBAR WITH TAILWIND STYLING
+    <nav className="mb-20 flex items-center justify-between py-4">
+        <div className="flex flex-shrink-0 items-center">
+            {/* <img src={logo} alt="James Eckman Logo" /> */}
+            <p className='text-cyan-50 font-bold font-sans hover:text-cyan-300 text-3xl ps-8'>jE</p>
+        </div>
+        {/* DISPLAY SOCIAL MEDIA ICONS WITH TAILWIND STYLING */}
+        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+            <FaLinkedin class='text-slate-600 hover:text-cyan-300' />
+            <FaGithub class='text-slate-600 hover:text-cyan-300' />
+            <FaInstagram class='text-slate-600 hover:text-cyan-300' />
+        </div>
+    </nav>        
+  )
 }
 
 export default Navbar;
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate brand new full stack developer with a knack for creating efficient and scalable web applications. Within the 6 months of hands-on experience I gained through the Bootcamp at Columbia Engineering, I have honed my skills in front-end technologies like React, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to learn from those who have been in the industry for years and work with them to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_TEXT = `I am a passionate brand new full stack developer with a knack for creating efficient and scalable web applications. Within the 6 months of hands-on experience I gained through the Bootcamp at Columbia Engineering, I have honed my skills in front-end technologies like React, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to learn from those who have been in the industry for years and work with them to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated, hard working and versatile full stack software engineer with a passion for creating efficient and user-friendly web applications along with an eye for creativity. Throughout my Columbia Engineering Bootcamp experience, I have worked with a variety of technologies, including React, Node.js, Express.js, MySQL, PostgreSQL, and MongoDB. My interest in web development began similarly to my career as an auto service technician... With a deep curiosity for how things work, and I'm hoping it will evolve into a career where I continue to strive to learn and adapt to new challenges. I excel in collaborative work environments and enjoy solving complex problems to deliver high-quality solutions. In addition to coding, I enjoy working out, exploring new technologies, and looking through open-source projects to improve my skills.`;
 
@@ -20,7 +20,7 @@ export const EXPERIENCES = [
 export const PROJECTS = [
   {
     title: "E-Commerce Website",
-    image: project1,
+    image: ,
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
     technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
