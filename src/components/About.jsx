707257717a4f6471649454f76b755377b5946126
jimmyboy722 <jeckman722@gmail.com>
2024-11-06 00:22:45 -0500

    fixed issue in hero.jsx file for background and created and added about me section with styling

---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import { ABOUT_TEXT } from "../constants";
+
+// import soloImg from "../assets/solo.jpg"
+const About = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+        <h2 className="my-20 text-center text text-4xl">About <span className="text-slate-600 hover:text-cyan-300">Me</span>
+        </h2>
+        <div className="flex flex-wrap">
+            <div className="w-full lg:w-1/2 lg:p-8">
+                <div className="flex items-center justify-center">
+                    <img className='rounded-2xl'src='../assets/projects/solo.jpg' alt="Pic of James Eckman"/>
+                </div>
+            </div>
+                <div className="w-full lg:w-1/2">
+                    <div className="flex justify-center lg:justify-start">
+                        <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
+                    </div>
+                </div>
+        </div>
+    </div>
+  )
+}
+
+export default About;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,14 +9,14 @@ const Hero = () => {
                 <div className="flex flex-col items-center lg:items-start">
                     <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">James Eckman</h1>
                     <span className="bg-gradient-to-r from-cyan-300 via-slate-500 to-blue-500 bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Web Developer</span>
-                    <p className="">
+                    <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
                       {HERO_TEXT}
                     </p>
                 </div>
             </div>
               <div className="w-full lg:w-1/2 lg:p-8">
                <div className="flex justify-center">
-                <img src={buddy} alt="James and Logan"/>
+                <img className="rounded-md size-2/3" src={buddy} alt="James and Logan"/>
               </div>
             </div>
         </div>
