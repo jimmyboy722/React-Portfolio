f9626a5612c719a1e33c5ee0d56f3d4ca9dddcfd
jimmyboy722 <jeckman722@gmail.com>
2024-11-09 23:55:16 -0500

    added links to sections of page in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@
 import { FaLinkedin } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa';
 import { FaInstagram } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
@@ -11,6 +12,12 @@ const Navbar = () => {
             {/* <img src={logo} alt="James Eckman Logo" /> */}
             <p className='text-cyan-50 font-bold font-sans hover:text-cyan-300 text-3xl ps-8'>jE</p>
         </div>
+        <ol className="flex flex-shrink-0 items-center gap-4 text-slate-600">
+          <li className="m-8"><Link to className='hover:text-cyan-300'>About</Link></li>
+          <li className="m-8"><Link to className='hover:text-cyan-300'>Technologies</Link></li>
+          <li className="m-8"><Link to className='hover:text-cyan-300'>Projects</Link></li>
+          <li className="m-8"><Link to className='hover:text-cyan-300'>Contact</Link></li>
+        </ol> 
         {/* DISPLAY SOCIAL MEDIA ICONS WITH TAILWIND STYLING */}
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
             <FaLinkedin class='text-slate-600 hover:text-cyan-300' />
